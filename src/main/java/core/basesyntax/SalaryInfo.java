aee42c7e1b4f9816584b38ff7755a61c242b2521
AlexPranschke <alexpranschke@gmail.com>
2026-04-07 21:37:36 +0200

    fix magic number lane separator

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,13 +5,17 @@ import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final int DATE_INDEX = 0;
+    public static final int NAME_INDEX = 1;
+    public static final int HOURS_INDEX = 2;
+    public static final int SALARY_INDEX = 3;
 
     public String getSalaryInfo(String[] names,
                                 String[] data,
                                 String dateFrom,
                                 String dateTo) {
         StringBuilder builder = new StringBuilder();
-        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append("\n");
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
 
@@ -20,14 +24,14 @@ public class SalaryInfo {
             int salary = 0;
             for (String record : data) {
                 String[] recordData = record.split(" ");
-                LocalDate recordDate = LocalDate.parse(recordData[0], formatter);
-                if (recordData[1].equals(name)
+                LocalDate recordDate = LocalDate.parse(recordData[DATE_INDEX], formatter);
+                if (recordData[NAME_INDEX].equals(name)
                         && (recordDate.isAfter(from) || recordDate.isEqual(from))
                         && (recordDate.isBefore(to) || recordDate.isEqual(to))) {
-                    salary += Integer.parseInt(recordData[2]) * Integer.parseInt(recordData[3]);
+                    salary += Integer.parseInt(recordData[HOURS_INDEX]) * Integer.parseInt(recordData[SALARY_INDEX]);
                 }
             }
-            builder.append(salary).append("\n");
+            builder.append(salary).append(System.lineSeparator());
 
         }
         return builder.toString();
